807c53083699eb3b77ef4b9e5867288dc2064478
BuildTools <unconfigured@null.spigotmc.org>
2023-01-13 20:38:37 +0100

    First push

---
+++ SirGerbain_PrisonerEscape.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using FivePD.API;
+
+namespace SirGerbain_PrisonerEscape
+{
+    [CalloutProperties("Prisoner Escape", "sirGerbain", "1.0.0")]
+    public class SirGerbain_PrisonerEscape : FivePD.API.Callout
+    {
+        public SirGerbain_PrisonerEscape()
+        {
+
+        }
+
+        public async override Task OnAccept()
+        {
+
+        }
+
+        public async override void OnStart(Ped closest)
+        {
+
+        }
+    }
+}
